e3911dcc385cb0f958e099f808678d347a4d4010
Kylan Strope <kylanstrope@gmail.com>
2017-12-06 06:14:28 +0000

    ahah!

--- lab10/BellmanFord.cpp
+++ lab10/BellmanFord.cpp
@@ -11,18 +11,20 @@ struct edge {
 
 struct graph {
 	struct edge* arr;
-}
+};
 
 int main() {
 	int V, E, u, v, w;
 	cin >> V;
 	cin >> E;
+	//cout << "V: " << V << "E: " << E << endl;
 	struct graph* G = new graph;
 	int cost[V];
 	edge* edges = new edge[E];
-	bool cycleFound = FALSE;
+	bool cycleFound = false;
 	G->arr = edges;
 	for (int i = 0; i < E; i++) {
+		//cout << "loop: " << i << endl;
 		cin >> u;
 		cin >> v;
 		cin >> w;
@@ -30,34 +32,42 @@ int main() {
 		G->arr[i].v = v;
 		G->arr[i].w = w;
 	}
-	
-	for (i = 0; i < V; i++) {
-		if (i = 0)
+
+	for (int i = 0; i < V; i++) {
+		//cout << "loop2: " << i << endl;
+		if (i == 0)
 			cost[i] = 0;
 		else
 			cost[i] = INT_MAX;
 	}
-	
-	for (i = 1; i <= V-1; i++) {
+
+	for (int i = 1; i <= V-1; i++) {
+		//cout << "loop3: " << i << endl;
 		for (int j = 0; j < E; j++) {
+			//cout << "loop4: " << j << endl;
 			if (cost[G->arr[j].u] != INT_MAX && (cost[G->arr[j].u] + G->arr[j].w < cost[G->arr[j].v])) {
 				cost[G->arr[j].v] = cost[G->arr[j].u] + G->arr[j].w;
 			}
 		}
 	}
-	
-	for (i = 0; i < E; i++) {
-		if (cost[G->arr[i].src] != INT_MAX && (cost[G->arr[j].u] + G->arr[j].w < cost[G->arr[j].v])) {
-			cycleFound = TRUE;
+
+	for (int i = 0; i < E; i++) {
+		//cout << "loop5: " << i << endl;
+		if (cost[G->arr[i].u] != INT_MAX && (cost[G->arr[i].u] + G->arr[i].w < cost[G->arr[i].v])) {
+			cycleFound = true;
 		}
 	}
-	
-	if (cycleFound == TRUE) {
+
+	if (cycleFound == true) {
 		cout << "FALSE" << endl;
 	}
-	else if (cycleFound == FALSE){
-		for (i = 0; i < V; i++) {
-			cout << cost[i] << endl;
+	else if (cycleFound == false){
+		cout << "TRUE" << endl;
+		for (int i = 0; i < V; i++) {
+			if (cost[i] == INT_MAX)
+				cout << "INFINITY" << endl;
+			else
+				cout << cost[i] << endl;
 		}
 	}
 }
